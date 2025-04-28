10d37c12da56f4e7eea6ad36f911ddbdc8e10c5a
YassirJr <yassir.junior.03@gmail.com>
2025-04-28 15:36:56 +0100

    create dtos

---
+++ backend/src/main/java/com/digital_banking/backend/dtos/BankAccountDTO.java
@@ -0,0 +1,8 @@
+package com.digital_banking.backend.dtos;
+
+import lombok.Data;
+
+@Data
+public class BankAccountDTO {
+    private String type;
+}
---
+++ backend/src/main/java/com/digital_banking/backend/dtos/SavingBankAccountDTO.java
@@ -0,0 +1,17 @@
+package com.digital_banking.backend.dtos;
+
+import com.digital_banking.backend.enums.AccountStatus;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SavingBankAccountDTO extends
+        BankAccountDTO {
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    private CustomerDTO customerDTO;
+    private double interestRate;
+}
